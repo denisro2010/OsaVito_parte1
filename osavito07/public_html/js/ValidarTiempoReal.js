76571eef12ddd62507ab78d1444f67e7fc5733d7
denis <denis@DESKTOP-81U5CMS>
2017-11-18 15:27:43 +0100

    He añadido algunos controles para la validacion en tiempo real

--- osavito07/public_html/js/ValidarTiempoReal.js
+++ osavito07/public_html/js/ValidarTiempoReal.js
@@ -17,4 +17,22 @@ function controlarNumColegiado() {
     }
 }
 
+function controlarTIS() {
+    var elemento = document.getElementById("TIS");
+    if (elemento.validity.valid) {
+        elemento.style.background = '#FFFFFF';
+    } else {
+        elemento.style.background = '#FFDDDD';
+    }
+}
+
+function controlarTelefono() {
+    var elemento = document.getElementById("telefono");
+    if (elemento.validity.valid) {
+        elemento.style.background = '#FFFFFF';
+    } else {
+        elemento.style.background = '#FFDDDD';
+    }
+}
+
 

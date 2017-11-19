4e95eb6a0f2f7bd700db6fb48472680ae51cd580
denis <denis@DESKTOP-81U5CMS>
2017-11-19 21:44:53 +0100

    He hecho las tablas en indexedDB para los pacientes y los sanitarios.

---
+++ osavito07/public_html/js/indexedPaciente.js
@@ -0,0 +1,93 @@
+// JavaScript Document
+var bd = null;
+function iniciar(){
+	zonadatos=document.getElementById("zonadatos");
+	
+	boton=document.getElementById("btnalta");
+	
+	boton.addEventListener("click",agregarobjeto, false);
+ 
+        var solicitud=indexedDB.open("OsaVito07", 2);
+	
+	solicitud.onsuccess=function(e){
+		bd=e.target.result;				
+	};
+	
+	solicitud.onupgradeneeded=function(e){
+		bd=e.target.result;
+		bd.createObjectStore("pacientes", {keyPath: "TIS"});
+	};	
+}
+      
+function agregarobjeto(){
+        
+	var TIS=document.getElementById("TIS").value;
+	
+	var gnombre=document.getElementById("gnombre").value;
+	
+	var telefono=document.getElementById("telefono").value;
+        
+        var fecha=document.getElementById("fecha").value;
+        
+        var hombre=document.getElementById("hombre").value;
+        
+        var mujer=document.getElementById("mujer").value;
+	
+	var transaccion=bd.transaction(["pacientes"], "readwrite");
+	
+	var almacen=transaccion.objectStore("pacientes");
+        
+	       var agregar=almacen.add({TIS: TIS, gnombre: gnombre, telefono: telefono, fecha: fecha, hombre: hombre, mujer: mujer});
+               agregar.addEventListener("success", mostrar, false);
+               
+               agregar.onerror = function(e) {
+               alert(agregar.error.name + '\n\n' + agregar.error.message);
+               location.href="http://localhost:8383/osavito07/altaPacientes.html";
+               };
+         
+             
+	document.getElementById("TIS").value="";
+	
+	document.getElementById("gnombre").value="";
+	
+	document.getElementById("telefono").value="";
+        
+        document.getElementById("fecha").value="";
+        
+        document.getElementById("hombre").value="";
+        
+        document.getElementById("mujer").value="";
+}
+
+function mostrar(){
+	
+	zonadatos.innerHTML="";
+	
+	var transaccion=bd.transaction(["pacientes"],"readonly");
+	
+	var almacen=transaccion.objectStore("pacientes");
+	
+	var cursor=almacen.openCursor();
+	
+	cursor.addEventListener("success", mostrarDatos, false);	
+	
+}
+
+function mostrarDatos(e){
+	
+	var cursor=e.target.result;
+	
+	if(cursor){
+		
+		zonadatos.innerHTML+="<div>" + cursor.value.TIS + " - " + cursor.value.gnombre + " - " + cursor.value.telefono +" - " + cursor.value.fecha + " - " +cursor.value.hombre +" - " + cursor.value.mujer + "</div>";
+		
+		cursor.continue();
+		
+		
+	}
+
+}
+
+window.addEventListener("load", iniciar, false);
+
+
---
+++ osavito07/public_html/js/indexedSanitario.js
@@ -0,0 +1,82 @@
+// JavaScript Document
+var bd;
+function iniciar(){
+	zonadatos=document.getElementById("zonadatos");
+	
+	boton=document.getElementById("btnalta");
+	
+	boton.addEventListener("click",agregarobjeto, false);
+        
+	var solicitud=indexedDB.open("OsaVito07");
+	
+	solicitud.onsuccess=function(e){
+		bd=e.target.result;				
+	};
+	
+	solicitud.onupgradeneeded=function(e){
+		bd=e.target.result;
+		bd.createObjectStore("sanitarios", {keyPath: "cnNumColegiado"});
+	};	
+	
+}
+      
+function agregarobjeto(){
+	
+	var cnNumColegiado=document.getElementById("cnNumColegiado").value;
+	
+	var gnombre=document.getElementById("gnombre").value;
+	
+	var tiposanitario=document.getElementById("tiposanitario").value;
+	
+	var transaccion=bd.transaction(["sanitarios"], "readwrite");
+	
+	var almacen=transaccion.objectStore("sanitarios");
+
+	var agregar=almacen.add({cnNumColegiado: cnNumColegiado, gnombre: gnombre, tiposanitario: tiposanitario});
+             
+        agregar.addEventListener("success", mostrar, false);
+ 
+         agregar.onerror = function(e) {
+         alert(agregar.error.name + '\n\n' + agregar.error.message);
+         location.href="http://localhost:8383/osavito07/altaSanitarios.html";
+         };
+         
+	document.getElementById("cnNumColegiado").value="";
+	
+	document.getElementById("gnombre").value="";
+	
+	document.getElementById("tiposanitario").value="";
+}
+
+function mostrar(){
+	
+	zonadatos.innerHTML="";
+	
+	var transaccion=bd.transaction(["sanitarios"],"readonly");
+	
+	var almacen=transaccion.objectStore("sanitarios");
+	
+	var cursor=almacen.openCursor();
+	
+	cursor.addEventListener("success", mostrarDatos, false);	
+	
+}
+
+function mostrarDatos(e){
+	
+	var cursor=e.target.result;
+	
+	if(cursor){
+		
+		zonadatos.innerHTML+="<div>" + cursor.value.cnNumColegiado + " - " + cursor.value.gnombre + " - " + cursor.value.tiposanitario + "</div>";
+		
+		cursor.continue();
+		
+		
+	}
+
+}
+
+window.addEventListener("load", iniciar, false);
+
+
